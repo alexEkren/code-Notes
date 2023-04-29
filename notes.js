fd345b3621ecbc27d1b3ca61f2b4fa8176da2d81
alexEkren <naelmehmetekren@gmail.com>
2023-04-29 08:50:04 +0300

    me trying to learn web development

---
+++ notes.js
@@ -0,0 +1,40 @@
+//  I am shit at taking notes 
+//DAY 1 AND 3 
+// <! -- DOCTYPE just tells the web that hay this file is useing HTML 5 -->
+
+// <!--TAGS THAT I SHOULD KNOW -->
+// <b> </b><!-- this is the same as <SRTONGE but we dont realy use b  any more becuse phones had a hard time prosesing it -->
+// <i> </i><!-- this is the same as <EM but it had the same problem as b -->
+// <em> </em><!--is used to define emphasized text -->
+// <!---->
+// <hr <!--The <hr> element is most often displayed as a horizontal rule that is used to separate content (or define a change) in an HTML page. -->
+// <br <!--The <br> tag inserts a single line break.The <br> tag is useful for writing addresses or poems. -->
+// <!--anchor tag lets you add links  <a href = ""></a>   -->
+
+
+// in HTML   we want to have evrything in a tag you can put text that is not in tag
+//     but it is preferred not to do that 
+
+// DAY 3 OF LEARING TO CODE  
+// FUCK ME AND MY SHITY NOTES i am rewatching the shit i learned to take notes 
+// vid 24 out of 408 hoping to finish in 2 weeks 
+
+//why index.html not any other name .. by default most servers just say,
+// "I'll return 'index.html'- if I see 'index.html' so index.html is the home page 
+//but you can change that 
+ 
+//done rewatching section 4 on to section 5 
+
+//HTML forms 
+// day 4 i am moveing sooo slow but i am trying to get my shit life in order 
+// finishing up with html 
+// The <div> tag defines a division or a section in an HTML document
+
+//  4/23 / 2023 3:23 am  today is idk what day its been around a week and i feel like shit i dont want do do anything but at the dame time i know i need to 
+// my sleep is messed up i am trying to fix it. But i am going to stop delaying my work and start today I HOPE 
+
+
+
+
+
+
